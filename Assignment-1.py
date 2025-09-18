d16bfb71abd291d6972665d9d509df2219a319ab
Ravikiran <ravikiransv169@gmail.com>
2025-09-18 22:53:50 +0530

    Assignment 1

---
+++ Assignment-1.py
@@ -0,0 +1,16 @@
+#Task 1
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+print("addition: ",a + b )
+print("subtraction: ",a - b )
+print("multiplication: ",a * b )
+print("division: ",a / b )
+
+
+#Task 2
+
+first = input("Enter your first name: ")
+last = input("Enter your last name: ")
+
+print("Hello,",first + last ,"Welcome to the Python Programming.")
